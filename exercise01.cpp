9106e8f2c94ef3918122e4506b4b015437f0c047
Dinusha <99312176+it21213212@users.noreply.github.com>
2022-02-23 05:56:20 +0000

    Completed exercise 1

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
+ cout << "No\t" << "Name\t\t" << "Marks" << endl;
  for (int r = 0; r < 5; r++) {
      cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
+       << "\t"  // add the tab space
+       <<names[r]<< "\t\t"  // add the two tab spaces
+       << setiosflags(ios::fixed) << setprecision(2)   // add the fprmating outputs
+       <<marks[r] << endl;
  }
 }
