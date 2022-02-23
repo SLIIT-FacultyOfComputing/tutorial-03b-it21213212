0d18f6fa2f3cf5ca3c10a51561ee8d9e92908c0f
Dinusha <99312176+it21213212@users.noreply.github.com>
2022-02-23 06:01:36 +0000

    Completed exercise 2

--- exercise02.cpp
+++ exercise02.cpp
@@ -8,26 +8,30 @@ int main() {
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
     
-    cout << "Enter Box 1 Height : ";
+    cout << "Enter Box 1 Height : "; // get input from user
     cin >> box1Height;
-    cout << "Enter Box 1 Width : ";
+    cout << "Enter Box 1 Width : ";  // get input from user
     cin >> box1Width;
-    cout << "Enter Box 1 Length : ";
+    cout << "Enter Box 1 Length : ";  // get input from user
     cin >> box1Length;
     
-     cout << "Enter Box 2 Height : ";
+     cout << "Enter Box 2 Height : ";  // get input from user
     cin >> box2Height;
-    cout << "Enter Box 2 Width : ";
+    cout << "Enter Box 2 Width : ";  // get input from user
     cin >> box2Width;
-    cout << "Enter Box 2 Length : ";
+    cout << "Enter Box 2 Length : ";  // get input from user
     cin >> box2Length;
     
     totalVolume = volume(box1Height, box1Width, box1Length)
-             + volume(box2Height, box2Width, box2Length);
+             + volume(box2Height, box2Width, box2Length);  // calculate the volume
              
-    cout << "Volume of Box is " << totalVolume << endl;
+    cout << "Volume of Box is " << totalVolume << endl;  // print the value
     
     return 0;
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length)
+{
+  return height * width * length; // return the volume 
+}
